171f5f116f8c33ae4ab218164f94b3200b363deb
Subodh Lonkar <62687864+learner-subodh@users.noreply.github.com>
2021-01-20 19:12:55 +0530

    Update kkbox.py

--- kkbox.py
+++ kkbox.py
@@ -69,7 +69,7 @@ data_load_state = st.text('Loading data...')
 data = load_data()
 data_load_state.text("Done!")
 
-if st.checkbox('Show Raw Data'):
+if st.checkbox('Show a Subset of Raw Data'):
     st.subheader('Raw Data')
     st.write(data)
 
