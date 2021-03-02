918adad93b9273250b443d916427610ce75372b2
Subodh Lonkar <62687864+learner-subodh@users.noreply.github.com>
2021-03-02 14:36:18 +0530

    Update food-101.py

--- food-101.py
+++ food-101.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('**Ohh, we might be working on bringing some more new interesting features for you!**')
-st.subheader('**We will be back shortly :)**')
+st.subheader('**_We will be back shortly :)_**')
