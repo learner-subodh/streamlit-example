dfaf5c1e22613d01fbee205007c00e27a9d73cb0
Subodh Lonkar <62687864+learner-subodh@users.noreply.github.com>
2021-03-02 20:13:40 +0530

    Update food-101.py

--- food-101.py
+++ food-101.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("**Hey, it's Subodh. It happens to be that I am working on adding some more new interesting features!_**")
+st.title("**_Hey, it's Subodh. It happens to be that I am working on adding some more new interesting features!_**")
 st.subheader('**_Normal proceedings will resume shortly. Thanks for visiting :)_**')
